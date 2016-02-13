1a8bf2b62885e6b91779939662feb367753e6c86
179151830 <179151830@qq.com>
2016-02-12 22:25:22 -0800

    initial draft
    initial draft

---
+++ server/init_posts.js
@@ -0,0 +1,47 @@
+/**
+ * Created by kieky on 2/12/2016.
+ */
+// if the database is empty on server start, create some sample data.
+Meteor.startup(function () {
+    if (Tags.find().count() === 0) {
+        var tags = [
+            {tag: "Meteor",
+                count: 2
+            },
+            {tag: "Languages",
+                count: 1
+            }
+        ];
+        var blogs = [
+            {
+                title: "post one",
+                body: "body of post one",
+                user: "kieky",
+                permalink: "post1_kieky",
+                tags : [
+                    "Meteor",
+                    "Languages"
+                ]
+            },
+            {
+                title: "post two",
+                body: "body of post two",
+                user: "kieky",
+                permalink: "post2_kieky",
+                tags : [
+                    "Meteor"
+                ]
+            }
+        ];
+
+        var timestamp = new Date();
+        _.each(tags, function(tags) {
+            var tag_id = Tags.insert({tag: tags.tag,
+                count: tags.count});
+        });
+        _.each(blogs, function(blog) {
+            var blog_id = Posts.insert({title: blog.title,
+                body: blog.body, user: blog.user, permalink: blog.permalink, tags: blog.tags, date: timestamp});
+        });
+    }
+});
